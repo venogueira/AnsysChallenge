2d32606d3e80660dd58a3d1db73ffee5f667c4a0
venogueira <vitor.brochado@hotmail.com>
2025-03-07 11:44:42 +0000

    add private subnet add sg and rules for inbound traffic port 80 and all outbound traffic

--- main.tf
+++ main.tf
@@ -6,6 +6,13 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 }
 
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+}
+
 resource "aws_subnet" "public" {
   count = length(var.public_subnets)
   vpc_id = aws_vpc.vpc.id
@@ -28,6 +35,22 @@ resource "aws_route_table_association" "public" {
   count          = length(var.public_subnets)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "nginx_sg" {
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
